17ea45cb1c3d61896ee77ebcffad55a5cf2def34
Carlos Diaz <carlos.cedp@gmail.com>
2022-01-14 10:36:55 -0400

    chapter 17 finished

--- Controllers/ExampleController.cs
+++ Controllers/ExampleController.cs
@@ -11,7 +11,38 @@ namespace ControllersAndActions.Controllers
         // GET: Example
         public ActionResult Index()
         {
+            ViewBag.Message = TempData["Message"];
+            ViewBag.Date = TempData["Date"];
+            DateTime time = (DateTime)TempData.Peek("Date");
+            // You can preserve a value that would otherwise be deleted by using the "Keep" method
+            TempData.Keep("Date");
             return View("HomePage");
         }
+
+        public HttpStatusCodeResult StatusCode()
+        {
+            // return HttpNotFound();
+            // return new HttpUnauthorizedResult();
+            return new HttpStatusCodeResult(404, "URL cannot be serviced");
+        }
+
+        public RedirectToRouteResult Redirect()
+        {
+           // return RedirectToAction("Index"); 
+            return RedirectToRoute(new
+            {
+                controller = "Example",
+                action = "Index",
+                ID = "MyID"
+            });
+        }
+
+        public RedirectToRouteResult RedirectToRoute()
+        {
+            TempData["Message"] = "Hello";
+            TempData["Date"] = DateTime.Now;
+            return RedirectToAction("Index");
+        }
+
     }
 }
